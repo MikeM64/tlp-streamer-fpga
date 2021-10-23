ab6b3882d085bdcb4804bcc7c856b606d3a1b107
MikeM64 <mikem64@gmail.com>
2021-10-23 00:34:46 -0400

    Properly handle the tristate bus and close timing

--- src/design/tlp_streamer_top.vhd
+++ src/design/tlp_streamer_top.vhd
@@ -23,55 +23,122 @@ architecture RTL of tlp_streamer is
 
 type rx_usb_state is (RX_IDLE, RX_READY, RX_START, RX_WORD, RX_END);
 signal current_rx_state, next_rx_state: rx_usb_state;
-signal ft601_rxf_n_s: std_logic;
+
+signal ft601_be_rd_i: std_logic_vector(3 downto 0);
+signal ft601_data_rd_i: std_logic_vector(31 downto 0);
+
+signal ft601_be_wr_o: std_logic_vector(3 downto 0);
+signal ft601_data_wr_o: std_logic_vector(31 downto 0);
+
+signal ft601_bus_wr_s: std_logic;
+
+signal usr_rst_s: std_logic;
+signal fifo_rx_wr_data_s: std_logic_vector(35 downto 0);
+signal fifo_rx_rd_data_s: std_logic_vector(35 downto 0);
+signal fifo_rx_rd_en_s: std_logic;
+signal fifo_rx_wr_en_s: std_logic;
+signal fifo_rx_wr_full_s: std_logic;
+signal fifo_rx_rd_empty_s: std_logic;
+
+component fifo_36_36_prim IS
+  PORT (
+    rst : IN STD_LOGIC;
+    wr_clk : IN STD_LOGIC;
+    rd_clk : IN STD_LOGIC;
+    din : IN STD_LOGIC_VECTOR(35 DOWNTO 0);
+    wr_en : IN STD_LOGIC;
+    rd_en : IN STD_LOGIC;
+    dout : OUT STD_LOGIC_VECTOR(35 DOWNTO 0);
+    full : OUT STD_LOGIC;
+    empty : OUT STD_LOGIC
+  );
+END component fifo_36_36_prim;
 
 begin
-rx_process: process(ft601_clk_i, ft601_rxf_n_s, ft601_rxf_n_i,
-                    usr_rst_n_i, current_rx_state, ft601_be_io)
+
+rx_usb_fifo: fifo_36_36_prim
+    port map (
+        rst => usr_rst_s,
+        wr_clk => ft601_clk_i,
+        rd_clk => ft601_clk_i,
+        din => fifo_rx_wr_data_s,
+        wr_en => fifo_rx_wr_en_s,
+        rd_en => fifo_rx_rd_en_s,
+        dout => fifo_rx_rd_data_s,
+        full => fifo_rx_wr_full_s,
+        empty => fifo_rx_rd_empty_s);
+
+clock_process: process(ft601_clk_i, usr_rst_n_i)
 begin
+
+    usr_rst_s <= '0';
+
+    ft601_rst_n_o <= usr_rst_n_i;
     if (usr_rst_n_i = '0') then
         current_rx_state <= RX_IDLE;
-        next_rx_state <= RX_IDLE;
-        ft601_rst_n_o <= '0';
-    elsif (ft601_clk_i'EVENT and ft601_clk_i = '0') then
+        usr_rst_s <= '1';
+    elsif (ft601_clk_i'EVENT and ft601_clk_i = '1') then
+        -- From the datasheet, it looks like signals are expected
+        -- to change on the falling edge of the clock and reads
+        -- are expected to occur on the rising edge.
         current_rx_state <= next_rx_state;
-        ft601_rst_n_o <= '1';
-        ft601_rxf_n_s <= ft601_rxf_n_i;
     end if;
 
+end process clock_process;
+
+bus_read_write: process(ft601_bus_wr_s, ft601_be_io, ft601_data_io)
+begin
+
+    if (ft601_bus_wr_s = '0') then
+        ft601_be_io <= "ZZZZ";
+        ft601_data_io <= "ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ";
+    else
+        ft601_be_io <= ft601_be_wr_o;
+        ft601_data_io <= ft601_data_wr_o;
+    end if;
+    ft601_be_rd_i <= ft601_be_io;
+    ft601_data_rd_i <= ft601_data_io;
+
+end process bus_read_write;
+
+rx_process: process(ft601_rxf_n_i, fifo_rx_wr_full_s, ft601_be_rd_i,
+                    current_rx_state)
+begin
+
+    -- Assume the state does not change by default
+    next_rx_state <= current_rx_state;
+
+    -- Assume the FPGA is not taking control of the FT601 bus
+    ft601_oe_n_o <= '1';
+    ft601_rd_n_o <= '1';
+    fifo_rx_wr_en_s <= '0';
+    ft601_bus_wr_s <= '0';
+
     case current_rx_state is
-        when RX_IDLE => -- NOP, until the FT601 informs the FPGA there's data
-            if (ft601_rxf_n_s = '0') then
+        when RX_IDLE =>
+            if (ft601_rxf_n_i = '0') then
                 next_rx_state <= RX_READY;
-            else
-                next_rx_state <= RX_IDLE;
             end if;
-            ft601_oe_n_o <= '1';
-            ft601_rd_n_o <= '1';
-        when RX_READY => -- Can't progress until the FIFO is empty (TBD)
-            next_rx_state <= RX_START;
-            ft601_oe_n_o <= '1';
-            ft601_rd_n_o <= '1';
+        when RX_READY =>
+            if (fifo_rx_wr_full_s = '0') then
+                next_rx_state <= RX_START;
+            end if;
         when RX_START =>
-            -- Assert OE to inform the FT601 to start transferring data
             ft601_oe_n_o <= '0';
-            ft601_rd_n_o <= '1';
             next_rx_state <= RX_WORD;
         when RX_WORD =>
-            -- ft601_rd_n must be asserted for each word being read
             ft601_oe_n_o <= '0';
             ft601_rd_n_o <= '0';
-            -- Combine both the data and the BE information
-            -- and store together in the FIFO
-            if (ft601_be_io < "1111" or ft601_rxf_n_s = '1') then
+            fifo_rx_wr_en_s <= '1';
+
+            if (ft601_be_rd_i < "1111" or ft601_rxf_n_i = '1' or fifo_rx_wr_full_s = '1') then
                 next_rx_state <= RX_END;
-            else
-                next_rx_state <= RX_WORD;
             end if;
         when RX_END =>
-            ft601_oe_n_o <= '1';
-            ft601_rd_n_o <= '1';
+            ft601_oe_n_o <= '0';
+            ft601_rd_n_o <= '0';
             next_rx_state <= RX_IDLE;
     end case;
+
 end process rx_process;
 end architecture RTL;
---
+++ src/sim/tlp_streamer_usb_rx_sim.vhd
@@ -0,0 +1,114 @@
+--
+-- TLP Streamer - USB RX Simulation
+--
+-- (c) MikeM64 - 2021
+--
+
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
+
+entity tlp_usb_rx_sim is end tlp_usb_rx_sim;
+
+architecture test of tlp_usb_rx_sim is
+    component tlp_streamer
+        port(
+            ft601_clk_i     : in    std_logic;
+            ft601_be_io     : inout std_logic_vector(3 downto 0);
+            ft601_data_io   : inout std_logic_vector(31 downto 0);
+            ft601_oe_n_o    : out   std_logic;
+            ft601_rxf_n_i   : in    std_logic;
+            ft601_rd_n_o    : out   std_logic;
+            ft601_rst_n_o   : out   std_logic;
+            usr_rst_n_i     : in    std_logic);
+    end component;
+
+signal test_ft601_clk: std_logic := '0';
+signal test_ft601_be: std_logic_vector(3 downto 0) := "0000";
+signal test_ft601_data: std_logic_vector(31 downto 0) := "00000000000000000000000000000000";
+
+signal test_ft601_bus_wr_s: std_logic := '1';
+signal test_ft601_be_rd_i: std_logic_vector(3 downto 0);
+signal test_ft601_data_rd_i: std_logic_vector(31 downto 0);
+signal test_ft601_be_wr_o: std_logic_vector(3 downto 0);
+signal test_ft601_data_wr_o: std_logic_vector(31 downto 0);
+
+signal test_ft601_oe_n: std_logic := '1';
+signal test_ft601_rxf_n: std_logic := '1';
+signal test_ft601_rd_n: std_logic := '1';
+signal test_ft601_rst_n: std_logic := '1';
+signal test_usr_rst_n: std_logic := '1';
+
+begin
+
+UUT: tlp_streamer
+    port map(
+        ft601_clk_i => test_ft601_clk,
+        ft601_be_io => test_ft601_be,
+        ft601_data_io => test_ft601_data,
+        ft601_oe_n_o => test_ft601_oe_n,
+        ft601_rxf_n_i => test_ft601_rxf_n,
+        ft601_rd_n_o => test_ft601_rd_n,
+        ft601_rst_n_o => test_ft601_rst_n,
+        usr_rst_n_i => test_usr_rst_n);
+
+test_ft601_clk <= not test_ft601_clk after 5ns;
+
+bus_write: process(test_ft601_clk) begin
+
+if (test_ft601_clk'EVENT and test_ft601_clk = '0') then
+    if (test_ft601_bus_wr_s = '1') then
+        test_ft601_be <= test_ft601_be_wr_o;
+        test_ft601_data <= test_ft601_data_wr_o;
+    else
+        test_ft601_be <= "ZZZZ";
+        test_ft601_data <= "ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ";
+    end if;
+
+    test_ft601_be_rd_i <= test_ft601_be;
+    test_ft601_data_rd_i <= test_ft601_data;
+end if;
+
+end process bus_write;
+
+tb: process begin
+
+test_usr_rst_n <= '0';
+wait for 100ns;
+
+test_usr_rst_n <= '1';
+wait for 100ns;
+
+report "FPGA reset complete";
+
+-- Refer to pg. 17 of the FT601 datasheet for the
+-- controller read timing diagram. This is what is
+-- being verified here.
+
+-- Assert RXF_N from the FT601 to indicate there is data available
+test_ft601_rxf_n <= '0';
+
+wait for 10ns;
+--assert test_ft601_oe_n = '0' report "The core has not asserted OE_N, 3 cycles after RXF_N" severity failure;
+
+wait for 10ns;
+test_ft601_bus_wr_s <= '1';
+test_ft601_be_wr_o <= "1111";
+test_ft601_data_wr_o <= "11111111111111111111111111111111";
+
+wait for 10ns;
+--assert test_ft601_rd_n = '0' report "The core has not asserted RD_N, 1 cycle after OE_N" severity failure;
+test_ft601_bus_wr_s <= '1';
+test_ft601_be_wr_o <= "0000";
+test_ft601_data_wr_o <= "00000000000000000000000000000000";
+
+test_ft601_rxf_n <= '1';
+
+wait for 10ns;
+--assert test_ft601_oe_n = '1' report "The core has not de-asserted OE_N, 1 cycle after RXF_N" severity failure;
+--assert test_ft601_rd_n = '1' report "The core has not de-asserted RD_N, 1 cycle after RXF_N" severity failure;
+report "Simulation complete!";
+
+end process tb;
+
+end architecture;
